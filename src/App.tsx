1b7eb291333e92883b4ff6a9d2bb479bbe1eedab
rodrigocandidodev <rodrigo_candido.contato@outlook.com>
2023-12-30 18:37:27 -0300

    main section with text areas and aside column added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Github } from "lucide-react";
+import { FileVideo, Github } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 export function App() {
 
   return (
@@ -20,6 +21,41 @@ export function App() {
         </div>
       </header>
       
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea 
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+            />
+            <Textarea 
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA..."
+              readOnly
+            />
+          </div>
+          <p className="text-sm text-muted-foreground">Lembre-se você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.</p>
+        </div>
+
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label 
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+            >
+              <FileVideo className="h-4 w-4"/>
+              Selecione um vídeo
+            </label>
+            <input 
+              type="file" 
+              id="video" 
+              accept="video/mp4"
+              className="sr-only" 
+            />
+            <Separator/>
+          </form>
+        </aside>
+      </main>
     </div>
   )
 }
